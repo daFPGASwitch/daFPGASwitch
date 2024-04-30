47335f9475f5f51f23f8bb4e69388211ae3096b6
tengjiang <jiangteng0403@gmail.com>
2024-04-29 21:59:41 -0400

    Update skeleton for scheduler. Update README.

--- hw/sched.sv
+++ hw/sched.sv
@@ -1,23 +1,26 @@
-module sched(input logic  [3:0] a,
-	       output logic [6:0] y);
-   always_comb
-      case (a)
-         4'h0: y = 7'b100_0000;
-         4'h1: y = 7'b111_1001;
-         4'h2: y = 7'b010_0100;
-         4'h3: y = 7'b011_0000;
-         4'h4: y = 7'b001_1001;
-         4'h5: y = 7'b001_0010;
-         4'h6: y = 7'b000_0010;
-         4'h7: y = 7'b111_1000;
-         4'h8: y = 7'b000_0000;
-         4'h9: y = 7'b001_0000;
-         4'hA: y = 7'b000_1000;
-         4'hB: y = 7'b000_0011;
-         4'hC: y = 7'b100_0110;
-         4'hD: y = 7'b010_0001;
-         4'hE: y = 7'b000_0110;
-         4'hF: y = 7'b000_1110;
-         default: y = 7'b111_1111;
-      endcase
+module sched (
+    input logic clk,
+    // input logic sched_en,
+    // input logic [3:0] is_busy,
+    // input logic [1:0] busy_voq_num_0,
+    // input logic [1:0] busy_voq_num_1,
+    // input logic [1:0] busy_voq_num_2,
+    // input logic [1:0] busy_voq_num_3,
+    input logic [3:0] voq_empty_0,
+    input logic [3:0] voq_empty_1,
+    input logic [3:0] voq_empty_2,
+    input logic [3:0] voq_empty_3,
+    output logic sched_sel_en,
+    output logic[3:0] sched_sel_0,
+    output logic[3:0] sched_sel_1,
+    output logic[3:0] sched_sel_2,
+    output logic[3:0] sched_sel_3
+);
+  always_ff @(posedge clk) begin
+    sched_sel_en <= 1;
+    sched_sel_0 <= voq_empty_0;
+    sched_sel_1 <= voq_empty_1;
+    sched_sel_2 <= voq_empty_2;
+    sched_sel_3 <= voq_empty_3;
+  end
 endmodule
--- hw/verilator/sched.cpp
+++ hw/verilator/sched.cpp
@@ -1,39 +1,66 @@
-
 #include <iostream>
-#include <iomanip>
 #include "Vsched.h"
 #include <verilated.h>
-
-unsigned char expected[] = { 0x40, 0x79, 0x24, 0x30,   // 0 - 3
-			     0x19, 0x12, 0x02, 0x78,   // 4 - 7
-			     0x00, 0x10, 0x08, 0x03,   // 8 - B
-			     0x46, 0x21, 0x06, 0x0e }; // C - F
+#include <verilated_vcd_c.h>
 
 int main(int argc, const char ** argv, const char ** env) {
-  int exitcode = 0;
-  
   Verilated::commandArgs(argc, argv);
 
-  Vsched * dut = new Vsched;  // Instantiate the collatz module
+  // Treat the argument on the command-line as the place to start
+  int n;
+  if (argc > 1 && argv[1][0] != '+') n = atoi(argv[1]);
+  else n = 7; // Default
 
-  for (int i = 0 ; i < 16 ; i++) {
-    dut->a = i;
-    dut->eval();
-    std::cout << std::hex << std::setfill('0') << std::setw(1) << (int) dut->a;
-    std::cout << ' ' <<  std::setfill('0') << std::setw(2) << (int) dut->y;
-    if (dut->y == expected[i])
-      std::cout << " OK";
-    else {
-      std::cout << " INCORRECT expected " << std::setfill('0') << std::setw(2)
-		<< (int) expected[i];
-      exitcode = 1;
-    }
-    std::cout << std::endl;
+  Vsched * dut = new Vsched;  // Instantiate the sched module
+
+  // Enable dumping a VCD file
+  
+  Verilated::traceEverOn(true);
+  VerilatedVcdC * tfp = new VerilatedVcdC;
+  dut->trace(tfp, 99); // Verilator should trace signals up to 99 levels deep
+  tfp->open("sched.vcd");
+
+  // Initial values
+  
+  dut->voq_empty_0 = 0;
+  dut->voq_empty_1 = 1;
+  dut->voq_empty_2 = 2;
+  dut->voq_empty_3 = 3;
+
+  // std::cout << dut->n; // Print the starting value of the sequence
+
+  bool last_clk = true;
+  int time;
+  for (time = 0 ; time < 10000 ; time += 10) {
+    dut->clk = ((time % 20) >= 10) ? 1 : 0; // Simulate a 50 MHz clock
+    // if (time == 20) dut->go = 1; // Pulse "go" for two cycles
+    // if (time == 30) dut->go = 0;
+
+    dut->eval();     // Run the simulation for a cycle
+    tfp->dump(time); // Write the VCD file for this cycle
+
+    // if (dut->clk && !last_clk && !dut->go) {
+    //   if (time >= 60) std::cout << ' ' << dut->dout; // Print the next value
+    //   if (dut->done) break; // Stop once "done" appears
+    // }
+    last_clk = dut->clk;
+  }
+
+  std::cout << std::endl;
+
+  // Once "done" is received, run a few more clock cycles
+  
+  for (int k = 0 ; k < 4 ; k++, time += 10) {
+    dut->clk = ((time % 20) >= 10) ? 1 : 0;
+      dut->eval();
+      tfp->dump(time);
   }
   
+  tfp->close(); // Stop dumping the VCD file
+  delete tfp;
+
   dut->final(); // Stop the simulation
   delete dut;
 
-  return exitcode;
+  return 0;
 }
-
