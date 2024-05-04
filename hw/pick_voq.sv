8ecdef4aaf5ecfb3aba7efcb9480f81105c0ad35
tengjiang <jiangteng0403@gmail.com>
2024-05-04 00:15:05 -0400

    Update pick_voq

--- hw/pick_voq.sv
+++ hw/pick_voq.sv
@@ -5,18 +5,17 @@ module pick_voq (
   output logic no_available_voq, // either all empty, or non-empty egress is taken.
   output logic [1:0] voq_to_pick
 );
-  logic [2:0] check_index;
-  logic [2:0] i;
+  logic [1:0] check_index;
+  logic [1:0] i;
   always_comb
     if (voq_empty == 4'b1111) begin
       no_available_voq = 1'b1;
     end else begin 
       no_available_voq = 1'b1;
-      for (i = 0; i < 4; i = i + 1) begin
-        check_index = start_voq_num + i[1:0];
-        check_index = (check_index > 3)? (check_index - 4): check_index;
-        if (voq_empty[check_index[1:0]] == 1'b0 && voq_picked[check_index[1:0]] == 1'b0) begin
-          voq_to_pick = check_index[1:0]; // Save the index of the first non-zero bit
+      for (i = 0; i <= 3; i = i + 1) begin
+        check_index = start_voq_num + i;
+        if (voq_empty[check_index] == 1'b0 && voq_picked[check_index] == 1'b0) begin
+          voq_to_pick = check_index; // Save the index of the first non-zero bit
           no_available_voq = 1'b0;
           break; // Exit the loop once the first non-empty, non-occupied egress is found
         end
