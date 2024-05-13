db3e25171f22cffab400382947e609ac1bd17132
tengjiang <jiangteng0403@gmail.com>
2024-05-13 03:05:51 -0400

    make ioctl slower.

--- fixed_length/sw/simpleSwitch.c
+++ fixed_length/sw/simpleSwitch.c
@@ -89,7 +89,7 @@ void set_ctrl_register(const packet_ctrl_t *pkt_ctrl)
 
 void send_packet(const packet_meta_t *pkt_meta)
 {
-    sleep(0.5);
+    sleep(1);
     if (ioctl(simple_switch_fd, SIMPLE_WRITE_PACKET, pkt_meta) < 0) {
         perror("Failed to send packet");
         return;
