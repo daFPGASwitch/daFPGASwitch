42c453f30db26d8e7840dee68fca216640b9a63d
tengjiang <jiangteng0403@gmail.com>
2024-05-13 18:22:09 -0400

    simple

--- fixed_length/sw/simpleSwitch.c
+++ fixed_length/sw/simpleSwitch.c
@@ -1,24 +1,3 @@
-/*
- * TODO: 
- * [ ] write to CTRL register (CTRL=1)
- *     inputting meta data
- * [ ] start w/ single threaded test prog
- * [ ] send 2 packets (call 2 ioctl w/ write + data flags & other combinations)
- * [ ] try setting different parameters using ioctl (i.e., src, dest, len)
- * [ ] len=1 or 2
- * [ ] ioctl_write (set CTRL=2) for both read & write
- * [ ] ioctl_read should return the 2 packets
- *    (1) inf loop to read packets: can i write at the same time???
- *     OR
- *    (2) ioctl w/ different flags
- *    (3)  
- *     [ ] 
- * [ ] later: create separate thread for polling
- *
- * ioctrl_write w/ write flag (1) and data and actual packet metadata that we assemble here
- * all port 0 (src + dest)
- */
-
 #include <stdio.h>
 #include "simpleSwitch.h"
 #include <sys/ioctl.h>
@@ -28,17 +7,16 @@
 #include <string.h>
 #include <unistd.h>
 
-#define MIN_PORT_NUM 0
-#define MAX_PORT_NUM 3
 #define DEVICE_PATH "/dev/simple_driver"
-
-
 int simple_switch_fd;
 
-// High [2 bit src] [2 bit dst] [6 bits for the # of 32 byte chunks] [22 bit time stamp] Low
+// High [2 bit src] [2 bit dst] [6 bits for the # of 32 byte chunks] [11 bit start time] [11 bit end time] Low
 // typedef unsigned int packet_meta_t;
 // typedef unsigned int packet_ctrl_t;
 // packet_info_t;
+
+int num_sent = 0;
+int num_get = 0;
 
 void open_simple_device()
 {
@@ -49,39 +27,33 @@ void open_simple_device()
     }
 }
 
-void print_packet_no_hw(void *packet_data)
-{
-	unsigned int packet = *((unsigned int*) packet_data);
-	
-    printf("\tmetadata (0x%X): [%u | %u | %u | %u]\n",
-		packet,
-        (packet >> 30) & 0x3,  // Extract bits 31:30
-        (packet >> 28) & 0x3,  // Extract bits 29:28
-        (packet >> 22) & 0x3F, // Extract bits 27:22
-         packet & 0x3FFFFF     // Extract bits 21:0
-    );
-}
-
 void print_packet(void *packet_data)
 {
 	unsigned int packet = *((unsigned int*) packet_data);
-    if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_0, packet_data)) {
-        perror("ioctl(SIMPLE_READ_PACKET_0) failed");
-        return;
-    }
-
-    printf("\tmetadata: [%u | %u | %u | %u]\n",
+	
+    printf("\tmetadata: [%u | %u | %u | %u | %u ]\n",
         (packet >> 30) & 0x3,  // Extract bits 31:30
         (packet >> 28) & 0x3,  // Extract bits 29:28
         (packet >> 22) & 0x3F, // Extract bits 27:22
-         packet & 0x3FFFFF     // Extract bits 21:0
+        (packet >> 11) & 0x7FF, // Extract bits 21:11
+         packet & 0x7FF     // Extract bits 10:0
     );
+}
+
+int extra_time_delta(unsigned int packet)
+{
+    return (packet & 0x7FF) - ((packet >> 11) & 0x7FF);
+}
+
+int extra_dst_port(unsigned int packet)
+{
+    return (packet >> 28) & 0x3;
 }
 
 void set_ctrl_register(const packet_ctrl_t *pkt_ctrl)
 {
 	if (ioctl(simple_switch_fd, SIMPLE_WRITE_CTRL, pkt_ctrl) < 0) {
-        perror("ioctl(SIMPLE_WRITE_CTRL) set CTRL=1 failed\n");
+        perror("ioctl(SIMPLE_WRITE_CTRL) set CTRL failed\n");
         close(simple_switch_fd);
         return;
     }
@@ -89,11 +61,12 @@ void set_ctrl_register(const packet_ctrl_t *pkt_ctrl)
 
 void send_packet(const packet_meta_t *pkt_meta)
 {
-    sleep(0.5);
+    usleep(5000);
     if (ioctl(simple_switch_fd, SIMPLE_WRITE_PACKET, pkt_meta) < 0) {
         perror("Failed to send packet");
         return;
     }
+    num_sent++;
 }
 
 // void receive_packet(int packet_id, packet_meta_t *pkt_meta)
@@ -135,80 +108,91 @@ void set_all_packet_fields(packet_meta_t *pkt_meta, unsigned int dst,
 
 int main()
 {
-    int write_num_packets = 2, num_sent = 0;
-	unsigned int dest = 0, src = 0, len = 1, t_delta = 10;
     packet_meta_t pkt_meta, rcvd_pkt_meta;
     packet_ctrl_t pkt_ctrl;
+    int total_latency = 0;
 
     open_simple_device();
 
-	printf("Set CTRL register to 0\n");
+	printf("Start: \n");
     pkt_ctrl = 0;
 	set_ctrl_register(&pkt_ctrl);
-	print_packet_no_hw(&pkt_ctrl);
+	print_packet(&pkt_ctrl);
+    usleep(1000);
 
     // Set control register (CTRL=1)
-	printf("Set CTRL register to 1\n");
+	printf("Start sending\n");
     pkt_ctrl = 1;
 	set_ctrl_register(&pkt_ctrl);
-	print_packet_no_hw(&pkt_ctrl);
+	print_packet(&pkt_ctrl);
+    usleep(1000);
 
-    for (int i = 0; i < 10; i++) {
-        set_all_packet_fields(&pkt_meta, i%2, i%4, 4);
-		send_packet(&pkt_meta);
-		print_packet_no_hw(&pkt_meta);
-	}
-	num_sent += write_num_packets;
-
-	len = 2;
-
-	set_all_packet_fields(&pkt_meta, 0, 4, 4);
-    for (int i = 0; i < 10; i++)
-        set_all_packet_fields(&pkt_meta, i%4, (i+1)%4, 4);
+    for (int i = 0; i < 12; i++) {
+        set_all_packet_fields(&pkt_meta, (i+3)%4, (i+1)%4, 1); 
         send_packet(&pkt_meta);
-		print_packet_no_hw(&pkt_meta);
-	num_sent += write_num_packets;
+		print_packet(&pkt_meta);
+    }
 
-	printf("Set CTRL register to 2\n");
+    usleep(10000);
+	printf("Start recving\n");
 	pkt_ctrl = 2;
 	set_ctrl_register(&pkt_ctrl);
-	print_packet_no_hw(&pkt_ctrl);
+	print_packet(&pkt_ctrl);
+    usleep(10000);
 
 	printf("Requested %d packets\n", num_sent);
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 100; i++) {
+        usleep(1000);
     	if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_0, &rcvd_pkt_meta) < 0) {
 			perror("ioctl read packet failed");
 			close(simple_switch_fd);
 			return -1;
 		}
-        sleep(0.5);
-        printf("Port 0\n");
-		print_packet_no_hw(&rcvd_pkt_meta);
-            	if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_1, &rcvd_pkt_meta) < 0) {
+        if (rcvd_pkt_meta >> 22) {
+            printf("Port 0: \n");
+            print_packet(&rcvd_pkt_meta);
+            total_latency += extra_time_delta(rcvd_pkt_meta);
+            num_get++;
+        }
+        usleep(1000);
+        if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_1, &rcvd_pkt_meta) < 0) {
 			perror("ioctl read packet failed");
 			close(simple_switch_fd);
 			return -1;
 		}
-        sleep(0.5);
-        printf("Port 1\n");
-		print_packet_no_hw(&rcvd_pkt_meta);
-            	if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_2, &rcvd_pkt_meta) < 0) {
+        if (rcvd_pkt_meta >> 22) {
+            printf("Port 1: \n");
+            print_packet(&rcvd_pkt_meta);
+            total_latency += extra_time_delta(rcvd_pkt_meta);
+            num_get++;
+        }
+        usleep(1000);
+        if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_2, &rcvd_pkt_meta) < 0) {
 			perror("ioctl read packet failed");
 			close(simple_switch_fd);
 			return -1;
 		}
-        sleep(0.5);
-        printf("Port 2\n");
-		print_packet_no_hw(&rcvd_pkt_meta);
-            	if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_3, &rcvd_pkt_meta) < 0) {
+        if (rcvd_pkt_meta >> 22) {
+            printf("Port 2: \n");
+            print_packet(&rcvd_pkt_meta);
+            total_latency += extra_time_delta(rcvd_pkt_meta);
+            num_get++;
+        }
+        usleep(1000);
+        if (ioctl(simple_switch_fd, SIMPLE_READ_PACKET_3, &rcvd_pkt_meta) < 0) {
 			perror("ioctl read packet failed");
 			close(simple_switch_fd);
 			return -1;
 		}
-		sleep(0.5);
-        printf("Port 3\n");
-		print_packet_no_hw(&rcvd_pkt_meta);
+        if (rcvd_pkt_meta >> 22) {
+            printf("Port 3: \n");
+            print_packet(&rcvd_pkt_meta);
+            total_latency += extra_time_delta(rcvd_pkt_meta);
+            num_get++;
+        }
 	}
+    printf("Got %d packets\n", num_get);
+    printf("Total latency: %d\n", total_latency);
 
     close(simple_switch_fd);
 	printf("Userspace program terminating\n");
